5bba936e1b254c4bbe23f2e2a66bd9e79718bad2
db2k <33785843+gazecube@users.noreply.github.com>
2026-09-17 16:33:26 -0700

    fam: add Linux compatibility declarations

---
+++ eoe/cmd/fam/linux_compat.h
@@ -0,0 +1,26 @@
+#ifndef FAM_LINUX_COMPAT_H
+#define FAM_LINUX_COMPAT_H
+
+#ifdef __linux__
+
+#include <limits.h>
+
+#ifndef NGROUPS_UMAX
+# ifdef NGROUPS_MAX
+#  define NGROUPS_UMAX NGROUPS_MAX
+# else
+#  define NGROUPS_UMAX 65536
+# endif
+#endif
+
+#ifndef SGI_NOHANG
+#define SGI_NOHANG 1
+#endif
+
+int sgikopt(const char *option, char *buf, int buflen);
+int syssgi(int request, ...);
+int _daemonize(int flags, int fd0, int fd1, int fd2);
+
+#endif
+
+#endif
